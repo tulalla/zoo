a07f358a945e8473d9f203a7f79670c529cd993f
alexey1978malyshev@gmail.com <alexey1978malyshev@gmail.com>
2023-08-01 15:34:51 +0300

    Initial commit

---
+++ Squirrel.java
@@ -0,0 +1,7 @@
+//
+public class Squirrel extends Animal{
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Squirrel {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
